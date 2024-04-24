dcc25a5dda18493523848519f8e95a525ec72de6
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-04-24 19:24:16 +0500

    navbar va home qismining responsive web designi tayyor

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 <h1>Ava website</h1>
+
+<!-- Core styles -->
+<p>Blue button</p>
+<p>Section name</p>
+<p>Section heading</p>
+<p>Section title</p>
---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const mainNav = document.getElementById("mainNav");
+
+  menuToggle.addEventListener("click", function () {
+    mainNav.classList.toggle("open");
+  });
+});
