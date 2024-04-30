cd0766467e52e630012ce0664e5f88d37cdbbd81
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-04-30 13:47:54 +0500

    Blog is ready

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
   menuToggle.addEventListener("click", function () {
     mainNav.classList.toggle("open");
   });
+
+  // Get all FAQ accordion headers
+  var accordionHeaders = document.querySelectorAll(".faq-header");
+
+  // Loop through each header and attach click event listener
+  accordionHeaders.forEach(function (header) {
+    header.addEventListener("click", function () {
+      // Toggle the 'hidden' class on the next sibling element with class 'faq-body'
+      this.nextElementSibling.classList.toggle("h-0");
+
+      // Toggle the '-' or '+' sign based on the 'hidden' class
+      var plusMinusIcon = this.querySelector(".faq-plus");
+      if (plusMinusIcon.textContent === "+") {
+        plusMinusIcon.textContent = "-";
+      } else {
+        plusMinusIcon.textContent = "+";
+      }
+    });
+  });
 });
