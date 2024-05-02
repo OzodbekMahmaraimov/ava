356b7a5c4e950578c85561f2b075b51f5eb872d9
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-05-02 19:32:16 +0500

    partners qismi tayyor

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
       ".testimonial-carousel__card"
     ).offsetWidth,
     carouselChildrens = [...carousel.children],
-    wrapper = document.querySelector(".testimonial")
+    wrapper = document.querySelector(".testimonial");
 
   let isDragging = false,
     startX,
@@ -77,30 +77,33 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 
   const autoPlay = () => {
-    if(window.innerWidth < 80) return;
-    timeOutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-  }
+    if (window.innerWidth < 80) return;
+    timeOutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2500);
+  };
   autoPlay();
 
   const infiniteScroll = () => {
-    if(carousel.scrollLeft === 0) {
+    if (carousel.scrollLeft === 0) {
       carousel.classList.add("no-transition");
-      carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+      carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
       carousel.classList.remove("no-transition");
-    } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+    } else if (
+      Math.ceil(carousel.scrollLeft) ===
+      carousel.scrollWidth - carousel.offsetWidth
+    ) {
       carousel.classList.add("no-transition");
       carousel.scrollLeft = carousel.offsetWidth;
       carousel.classList.remove("no-transition");
     }
 
-    clearTimeout(timeOutId)
-    if(!wrapper.matches(":hover")) autoPlay()
-  }
+    clearTimeout(timeOutId);
+    if (!wrapper.matches(":hover")) autoPlay();
+  };
 
   carousel.addEventListener("mousedown", dragStart);
   carousel.addEventListener("mousemove", dragging);
   document.addEventListener("mouseup", dragStop);
-  carousel.addEventListener("scroll", infiniteScroll)
-  wrapper.addEventListener("mouseenter", () => clearTimeout(timeOutId));  
-  wrapper.addEventListener("mouseleave", autoPlay)
+  carousel.addEventListener("scroll", infiniteScroll);
+  wrapper.addEventListener("mouseenter", () => clearTimeout(timeOutId));
+  wrapper.addEventListener("mouseleave", autoPlay);
 });
